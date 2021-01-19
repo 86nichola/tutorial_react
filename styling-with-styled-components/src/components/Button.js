05a391d1bcbb485e4f9ee1cd3f9c524d2b5932cf
nicholaKim <86nichola@gmail.com>
2021-01-19 23:40:05 +0900

    colorStyles..

---
+++ styling-with-styled-components/src/components/Button.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import { darken, lighten } from 'polished';
+
+const colorStyles = css`
+  ${({theme, color}) => {
+    const selected = theme.palette[color]
+    return css`
+      background: ${selected};
+      &:hover {
+        background: ${lighten(0.1, selected)};
+      }
+      &:active {
+        background: ${darken(0.1, selected)};
+      }
+    `;
+
+  }}
+
+`;
+
+const StyledButton = styled.button`
+/* 공통 스타일 */
+  display: inline-flex;
+  outline: none;
+  border: none;
+  border-radius: 4px;
+  color: white;
+  font-weight: bold;
+  cursor: pointer;
+  padding-left: 1rem;
+  padding-right: 1rem;
+  padding-top: 0.5rem;
+  padding-bottom: 0.5rem;
+
+  /* 크기 */
+  height: 2.25rem;
+  font-size: 1rem;
+
+  /* 색상 */
+  ${colorStyles}
+
+  /* 기타 */
+  & + & {
+    margin-left: 1rem;
+  }
+
+
+`;
+
+function Button({ children, color, ...rest }){
+return <StyledButton color={color} {...rest}>{children}</StyledButton>
+}
+
+Button.defaultProps = {
+  color: 'blue'
+};
+
+export default Button;
