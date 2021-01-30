40b2bb009d78fa1e50b85a92b7b096699fb73aab
nicholaKim <86nichola@gmail.com>
2021-01-30 23:52:38 +0900

    Dialog confirm popup

--- styling-with-styled-components/src/components/Button.js
+++ styling-with-styled-components/src/components/Button.js
@@ -76,7 +76,7 @@ const StyledButton = styled.button`
   padding-right: 1rem;
   padding-top: 0.5rem;
   padding-bottom: 0.5rem;
-  margin-right: 1rem;
+  margin-top: 1rem;
 
   /* 크기 */
   ${sizeStyle}
