1c9e44cb824796a847a48126adad81edf9aecc99
nicholaKim <86nichola@gmail.com>
2021-03-24 23:08:59 +0900

    firebase cors issue

--- api-integrate/src/Users.js
+++ api-integrate/src/Users.js
@@ -4,7 +4,7 @@ import useAsync from "./useAsync";
 
 async function getQuote() {
   const response = await axios.get(
-    "/v7/finance/quote?&symbols=BTC-USD,ETH-USD,BNB-USD,USDT-USD,ADA-USD,DOT1-USD,DOT2-USD,XRP-USD,LTC-USD,LINK-USD,THETA-USD,BCH-USD,USDC-USD,XLM-USD,LUNA1-USD,DOGE-USD,VET-USD,TRX-USD,ATOM1-USD,SOL1-USD,MIOTA-USD,AVAX-USD,XMR-USD,BSV-USD,EOS-USD&fields=regularMarketPrice,regularMarketChange,regularMarketChangePercent,regularMarketVolume"
+    "https://query1.finance.yahoo.com/v7/finance/quote?&symbols=BTC-USD,ETH-USD,BNB-USD,USDT-USD,ADA-USD,DOT1-USD,DOT2-USD,XRP-USD,LTC-USD,LINK-USD,THETA-USD,BCH-USD,USDC-USD,XLM-USD,LUNA1-USD,DOGE-USD,VET-USD,TRX-USD,ATOM1-USD,SOL1-USD,MIOTA-USD,AVAX-USD,XMR-USD,BSV-USD,EOS-USD&fields=regularMarketPrice,regularMarketChange,regularMarketChangePercent,regularMarketVolume"
   );
   debugger;
   return response.data.quoteResponse.result;
@@ -19,30 +19,32 @@ async function getUsers() {
 }
 
 function Users() {
-  // const [state, refetch] = useAsync(getQuote, {} || [], true);
-  // const { loading, error, data } = state;
-  const [state, refetch] = useAsync(getUsers, [], true);
-  const { loading, error, data: users } = state;
+  const [state, refetch] = useAsync(getQuote, [], true);
+  const { loading, error, data: coins } = state;
+  // const [state, refetch] = useAsync(getUsers, [], true);
+  // const { loading, error, data: users } = state;
 
   if (loading) return <div>로딩중..</div>;
   if (error) return <div>에러가 발생했습니다</div>;
-  if (!users) return <button onClick={refetch}>불러오기</button>;
-  // if (!data) return <button onClick={refetch}>불러오기</button>;
+  //if (!users) return <button onClick={refetch}>불러오기</button>;
+  if (!coins) return <button onClick={refetch}>불러오기</button>;
 
   return (
     <>
       <ul>
         {
+          /*
           users.map((user) => (
             <li key={user.id}>
               {user.username} ({user.name})
             </li>
           ))
-          /*data.map((item) => (
+          */
+          coins.map((item) => (
             <li key={item.symbol}>
               {item.symbol}({item.regularMarketChange * 100})
             </li>
-          ))*/
+          ))
         }
       </ul>
 
--- api-integrate/src/setupProxy.js
+++ api-integrate/src/setupProxy.js
@@ -1,11 +1,11 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
-  app.use(
-    "/v7",
-    createProxyMiddleware({
-      target: "https://query1.finance.yahoo.com",
-      changeOrigin: true,
-    })
-  );
+  // app.use(
+  //   "/v7",
+  //   createProxyMiddleware({
+  //     target: "https://query1.finance.yahoo.com",
+  //     changeOrigin: true,
+  //   })
+  // );
 };
